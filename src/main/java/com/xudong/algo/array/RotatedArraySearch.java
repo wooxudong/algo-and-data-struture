44b7cdfa0d69cb83b9917c748c033b8232e5ded6
Wu Xudong <wooxudong@gmail.com>
2020-07-31 17:21:05 +0800

    added leetcode-33: search in a rotated array

---
+++ src/main/java/com/xudong/algo/array/RotatedArraySearch.java
@@ -0,0 +1,43 @@
+package com.xudong.algo.array;
+
+/**
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ *
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * You may assume no duplicate exists in the array.
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ *
+ * Example 1:
+ *
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ * Example 2:
+ *
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ *
+ * @author wooxudong
+ */
+public class RotatedArraySearch {
+    public int search(int[] nums, int target) {
+       return binarySearchR(nums, 0, nums.length-1, target);
+    }
+
+
+    private int binarySearchR(int[] nums, int low, int high, int target) {
+        if(low > high) return -1;
+        int middle = low + (high - low) / 2;
+        if(target == nums[middle]){
+            return middle;
+        }
+        if(nums[low] <= nums[middle]){
+            if(target >= nums[low] && target < nums[middle]) return binarySearchR(nums, low, middle-1, target);
+            else return binarySearchR(nums, middle+1, high, target);
+        }else{
+            if(target > nums[middle] && target <= nums[high]) return binarySearchR(nums, middle+1, high, target);
+            else return  binarySearchR(nums, low, middle-1, target);
+        }
+    }
+
+}
---
+++ src/test/java/com/xudong/algo/array/SearchInRotatedArrayTest.java
@@ -0,0 +1,94 @@
+package com.xudong.algo.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SearchInRotatedArrayTest {
+
+    @Test
+    void shouldReturnIndex_ifNotRotated() {
+        //given
+        int[] nums = new int[]{0, 1, 2, 4, 5, 6, 7};
+        int target = 4;
+
+        //when
+        int index = new RotatedArraySearch().search(nums, 4);
+
+        //then
+        assertThat(index).isEqualTo(3);
+    }
+
+    @Test
+    void shouldReturnIndex_ifNotRotated2() {
+        //given
+        int[] nums = new int[]{0, 1, 2, 4, 5, 6, 7};
+
+        //when
+        int index = new RotatedArraySearch().search(nums, 7);
+
+        //then
+        assertThat(index).isEqualTo(6);
+    }
+
+    @Test
+    void shouldReturnIndex_ifNotRotated3() {
+        //given
+        int[] nums = new int[]{0, 1, 2, 4, 5, 6, 7};
+
+        //when
+        int index = new RotatedArraySearch().search(nums, 0);
+
+        //then
+        assertThat(index).isEqualTo(0);
+    }
+
+    @Test
+    void shouldReturnMinusOne_ifNotRotated() {
+        //given
+        int[] nums = new int[]{0, 1, 2, 4, 5, 6, 7};
+        int target = 4;
+
+        //when
+        int index = new RotatedArraySearch().search(nums, 3);
+
+        //then
+        assertThat(index).isEqualTo(-1);
+    }
+
+    @Test
+    void shouldReturnMinusOne_ifRotated() {
+        //given
+        int[] nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+
+        //when
+        int index = new RotatedArraySearch().search(nums, 3);
+
+        //then
+        assertThat(index).isEqualTo(-1);
+    }
+
+    @Test
+    void shouldReturnIndex_ifRotated() {
+        //given
+        int[] nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+
+        //when
+        int index = new RotatedArraySearch().search(nums, 6);
+
+        //then
+        assertThat(index).isEqualTo(2);
+    }
+
+    @Test
+    void shouldReturnIndex_ifRotated2() {
+        //given
+        int[] nums = new int[]{3,1};
+
+        //when
+        int index = new RotatedArraySearch().search(nums, 1);
+
+        //then
+        assertThat(index).isEqualTo(1);
+    }
+}
