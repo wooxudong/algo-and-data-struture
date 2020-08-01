8395f5ba04cf2ad853715790c64b0ecedd9f0bc4
Wu Xudong <wooxudong@gmail.com>
2020-08-01 15:52:27 +0800

    added leetcode-34: search a range in a non-descending array

---
+++ src/main/java/com/xudong/algo/ValidSudoku.java
@@ -0,0 +1,62 @@
+package com.xudong.algo;
+
+/**
+ * Leetcode-36: Valid Sudoku
+ * Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+ *  1. Each row must contain the digits 1-9 without repetition.
+ *  2. Each column must contain the digits 1-9 without repetition.
+ *  3. Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ *
+ * @author wooxudong
+ */
+public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        if(board == null) return false;
+        for (int i = 0; i < board[0].length; i++) {
+            if (!checkColumn(board, i) || !checkRow(board, i) || !checkSubBox(board, i)) return false;
+        }
+        return true;
+    }
+
+    boolean checkColumn(char[][] board, int index) {
+        int isValid = 0;
+        for (int i = 0; i < board[0].length; i++) {
+            char ch = board[i][index];
+            if (ch != '.') {
+                int mask = 1 << (ch - '0');
+                if ((mask & isValid) != 0) return false;
+                else isValid |= mask;
+            }
+        }
+        return true;
+    }
+
+    boolean checkRow(char[][] board, int index) {
+        int isValid = 0;
+        for (char c : board[index]) {
+            if (c != '.') {
+                int mask = 1 << (c - '0');
+                if ((mask & isValid) != 0) return false;
+                else isValid |= mask;
+            }
+        }
+        return true;
+    }
+
+    boolean checkSubBox(char[][] board, int index) {
+        int startRow = index / 3 * 3;
+        int startColumn = (index % 3) * 3;
+        int isValid = 0;
+        for (int r = startRow; r <= startRow + 2; r++) {
+            for (int c = startColumn; c <= startColumn + 2; c++) {
+                char ch = board[r][c];
+                if (ch != '.') {
+                    int mask = 1 << (ch - '0');
+                    if ((mask & isValid) != 0) return false;
+                    else isValid |= mask;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/com/xudong/algo/ValidSudokuTest.java
@@ -0,0 +1,71 @@
+package com.xudong.algo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ValidSudokuTest {
+
+    @Test
+    void validSudoku() {
+        char[][] input = new char[][]{
+                new char[]{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                new char[]{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                new char[]{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                new char[]{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                new char[]{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                new char[]{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                new char[]{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                new char[]{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                new char[]{'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+
+        //when
+        boolean isValid = new ValidSudoku().isValidSudoku(input);
+
+        //then
+        assertThat(isValid).isTrue();
+    }
+
+
+    @Test
+    void inValidSudoku() {
+        char[][] input = new char[][]{
+                new char[]{'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                new char[]{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                new char[]{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                new char[]{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                new char[]{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                new char[]{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                new char[]{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                new char[]{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                new char[]{'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+
+        //when
+        boolean isValid = new ValidSudoku().isValidSudoku(input);
+
+        //then
+        assertThat(isValid).isFalse();
+    }
+
+    @Test
+    void failedCase() {
+        char[][] board = new char[][]{
+                new char[]{'.', '.', '.', '.', '5', '.', '.', '1', '.'},
+                new char[]{'.', '4', '.', '3', '.', '.', '.', '.', '.'},
+                new char[]{'.', '.', '.', '.', '.', '3', '.', '.', '1'},
+                new char[]{'8', '.', '.', '.', '.', '.', '.', '2', '.'},
+                new char[]{'.', '.', '2', '.', '7', '.', '.', '.', '.'},
+                new char[]{'.', '1', '5', '.', '.', '.', '.', '.', '.'},
+                new char[]{'.', '.', '.', '.', '.', '2', '.', '.', '.'},
+                new char[]{'.', '2', '.', '9', '.', '.', '.', '.', '.'},
+                new char[]{'.', '.', '4', '.', '.', '.', '.', '.', '.'}};
+
+        //when
+        boolean isValid = new ValidSudoku().isValidSudoku(board);
+
+        //then
+        assertThat(isValid).isFalse();
+
+    }
+}
+
